7df96064c48dcf559f98a3dd9cb6c7cdbfb36ecb
Tomasz <40639418+TiPiKej@users.noreply.github.com>
2020-10-12 00:56:17 +0200

    .

--- Numeral System Converter/task/src/converter/Main.java
+++ Numeral System Converter/task/src/converter/Main.java
@@ -5,11 +5,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final Scanner scanner = new Scanner(System.in);
-        final int srcRadix = Integer.parseInt(scanner.nextLine());
-        final String srcNumber = scanner.nextLine();
-        final int tgRadix = Integer.parseInt(scanner.nextLine());
+
+        String srcRadix = "";
+        String srcNumber = "";
+        String tgRadix = "";
+
+        boolean isError = false;
+
+        if (scanner.hasNextLine()) srcRadix = scanner.nextLine();
+        else isError = true;
+
+        if (scanner.hasNextLine()) srcNumber = scanner.nextLine();
+        else isError = true;
+
+        if (scanner.hasNextLine()) tgRadix = scanner.nextLine();
+        else isError = true;
+
+        if (!srcRadix.matches("\\d+")) isError = true;
+        if (!srcNumber.matches("[a-z0-9.]+")) isError = true;
+        if (!tgRadix.matches("\\d+")) isError = true;
+
         scanner.close();
 
-        System.out.println(Number.NToN(srcNumber, srcRadix, tgRadix));
+        if (!isError) {
+            if (Integer.parseInt(srcRadix) <= 0 || Integer.parseInt(srcRadix) > 36) isError = true;
+            if (Integer.parseInt(tgRadix) <= 0 || Integer.parseInt(tgRadix) > 36) isError = true;
+        }
+
+        if (isError) System.out.println("error");
+        else System.out.println(Number.NToN(srcNumber, Integer.parseInt(srcRadix), Integer.parseInt(tgRadix)));
     }
 }
